36b6e990d15050c0d1d73092e950401d9b8f364e
Pruthvi K.L <tinku@Pruthvis-MacBook-Air.local>
2026-01-13 13:40:31 -0600

    First push

---
+++ practise1.py
@@ -0,0 +1,19 @@
+2+3
+print(2+3)
+pi = 3.14
+radius = 3.4
+area = pi * (radius**2)
+print(area)
+
+radius = area + 1
+print(radius)
+area = pi * (radius**2)
+print (area)
+
+length = 10
+width = 5
+area = length * width
+print(area)
+
+
+
